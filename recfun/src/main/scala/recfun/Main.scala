fedc3ecf8240c9598e9cc2807257bfe0e685674e
HybridRbt <littlejack0120@gmail.com>
2014-05-07 15:51:32 -0500

    Complete ex3
    using algorithm in book
    <Structure_and_Interpretation_of_Computer_Programs>

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -22,10 +22,28 @@ object Main {
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def num_p(acc: Int, chars: List[Char]): Int = {
+      if (chars.isEmpty) acc
+      else if (acc < 0) acc
+      else if (chars.head == '(') num_p(acc+1, chars.tail)
+      else if (chars.head == ')') num_p(acc-1, chars.tail)
+      else num_p(acc, chars.tail)
+    }
+
+    if (num_p(0, chars) == 0) true
+    else false
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money == 0) 1
+    else if (money < 0 || coins.isEmpty) 0
+    else {
+      countChange(money - coins.sortWith(_>_).head, coins) + countChange(money, coins.sortWith(_>_).tail)
+    }
+  }
 }
