6167fd2302b29f162f41577cf0950485874c6b34
HybridRbt <littlejack0120@gmail.com>
2014-05-17 23:50:07 -0500

    change the view to consider p as another set

--- funsets/src/main/scala/funsets/FunSets.scala
+++ funsets/src/main/scala/funsets/FunSets.scala
@@ -44,9 +44,7 @@ object FunSets {
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-
   /**
-   *
    * Note 2 self: only the elements in 's', no elements in 't' that are not in 's'
    */
   def diff(s: Set, t: Set): Set = {
@@ -57,7 +55,8 @@ object FunSets {
    * Returns the subset of `s` for which `p` holds.
    */
   def filter(s: Set, p: Int => Boolean): Set = {
-    (x: Int) => contains(s, x) && p(x)
+    //(x: Int) => contains(s, x) && p(x)
+    (x: Int) => contains(s, x) && contains(p, x)
   }
 
   /**
@@ -68,10 +67,14 @@ object FunSets {
   /**
    * Returns whether all bounded integers within `s` satisfy `p`.
    */
+  /**
+   * see p as another set, this translate into: if iterate through all inbound integer, every one contained in s is also contained in p, then
+   * true; else false. which means s is a subset of p. thus empty s should be considered subset of p and return true.
+   */
   def forall(s: Set, p: Int => Boolean): Boolean = {
     def iter(a: Int): Boolean = {
       if (a < -bound) true /** finished iteration and no false, so true */
-      else if (contains(s, a) && !p(a)) false  /** when a is in bound, test to see if it is in a, and if it is, if it satisfy p */
+      else if (contains(s, a) && !contains(p, a)) false  /** when a is in bound, test to see if it is in a, and if it is, if it satisfy p */
       else iter(a-1)  /** a satisfy the req, examine the next one */
     }
     iter(bound)
@@ -80,6 +83,9 @@ object FunSets {
   /**
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
+   */
+  /**
+   * see p as another set, this translate into: if there is at least one inbound integer in s that is also in p.
    */
   def exists(s: Set, p: Int => Boolean): Boolean = ???
 
